5340b55a0f8d4c0a57ab9130d9097ee757de2777
PommereningMartin <37418568+PommereningMartin@users.noreply.github.com>
2022-11-05 09:09:24 +0100

    bring in outh against twitch

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template, request
+import json
+import os
+
+import requests as requests
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, login_required
+
 from service.category_service import CategoryService
+from oauthlib.oauth2 import WebApplicationClient
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 catService = CategoryService()
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+CLIENT_ID = 'deeg4tvrgziwwqpfwqtdd37z5gxltr'
+CLIENT_SECRET = 'ifllbh3xvun6818en99t5b34jd90qv'
+REDIRECT_URL = 'http://localhost:5000/callback'
+AUTH_ENDPOINT = 'https://id.twitch.tv/oauth2/authorize'
+TOKEN_ENDPOINT = 'https://id.twitch.tv/oauth2/token'
+client = WebApplicationClient(CLIENT_ID)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
+    return redirect(url_for('login'))
+
+
+@app.route('/home')
+def home():  # put application's code here
     return render_template('index.html', categories=catService.get_categories())
 
 
@@ -24,6 +46,47 @@ def check_answer():
         given_answer = request.json.get('answer')
         return {'answer': catService.check_answer(question_id, given_answer)}
     return {'answer': None}
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    request_uri = client.prepare_request_uri(
+        AUTH_ENDPOINT,
+        redirect_uri='http://localhost:5000/callback',
+        scope=["openid"],
+    )
+    return redirect(request_uri)
+
+
+class User:
+    pass
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+@app.route('/callback', methods=['GET', 'POST'])
+def login_callback():
+    code = request.args.get("code")
+    foo = TOKEN_ENDPOINT + '?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&code=' + code + '&grant_type=authorization_code' + '&redirect_uri=' + REDIRECT_URL
+    print(foo)
+    token_response = requests.post(foo)
+    token = token_response.json()['access_token']
+    # Parse the tokens!
+    print(token_response.json())
+    print(code)
+    if token is None:
+        return redirect(url_for('home'))
+    return "<h3>ERROR!!!!!!!!!</>"
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    # logout_user()
+    return redirect(url_for("login"))
 
 
 if __name__ == '__main__':
---
+++ service/token_service.py
@@ -0,0 +1,40 @@
+import datetime
+from flask import jwt
+
+SECRET_KEY = '1234567890'
+
+
+def encode_auth_token(self, user_id):
+    """
+    Generates the Auth Token
+    :return: string
+    """
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(
+            payload,
+            SECRET_KEY,
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
+
+
+@staticmethod
+def decode_auth_token(auth_token):
+    """
+    Decodes the auth token
+    :param auth_token:
+    :return: integer|string
+    """
+    try:
+        payload = jwt.decode(auth_token, SECRET_KEY)
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Signature expired. Please log in again.'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log in again.'
